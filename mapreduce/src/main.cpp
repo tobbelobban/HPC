9bb307f58690384d16cd2293a83f4a6207c622f3
Johan <ledeus@live.com>
2019-05-30 20:27:54 +0200

    igatherv

--- mapreduce/src/main.cpp
+++ mapreduce/src/main.cpp
@@ -3,22 +3,38 @@
 #include "mapreduce.hpp"
 
 int main(int argc, char ** argv) {
-
-	if(argc < 3) {
-		return 0;
-	}
-
+	int world_rank, world_size;
 	MapReduce mp;
 
-	MPI_Init(&argc, &argv);
-		mp.init(argv[1]);
-		while(mp.remaining_read > 0) {
-			mp.read();
-			mp.map();
-			mp.reduce();
+	MPI_Init( &argc, &argv );
+	MPI_Comm_rank( MPI_COMM_WORLD, &world_rank );
+	MPI_Comm_size( MPI_COMM_WORLD, &world_size );
+	if( argc < 3 ) {
+		if(world_rank == MASTER) {
+			std::cout << "Usage: mpirun ./path-to-binary input.txt output.txt" << std::endl;
 		}
-		mp.write(argv[2]);
-		mp.cleanup();
+		MPI_Finalize();
+		exit(1);
+	}
+
+	mp.init(argv[1]);
+
+	// continue reading -> mapping until out of data
+	while(mp.remaining_read > 0) {
+		mp.read();
+		mp.map();
+	}
+	// finally we reduce
+	mp.reduce();
+
+	// then write
+	mp.write(argv[2]);
+
+	// cleanup
+	mp.cleanup();
+
+	// à la fin
 	MPI_Finalize();
+
 	return 0;
 }
